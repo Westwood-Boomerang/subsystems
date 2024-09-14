2c3904113aa1f77ca13d0def35d11ff68bd27720
Perry <atharv.sondhi@gmail.com>
2024-09-14 13:06:02 -0500

    Didn't add the files for some reason???

---
+++ clawServo.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class clawServo {
+    Servo claw;
+    public clawServo(HardwareMap hardwareMap, String Name, boolean reverse, double rangeMin, double rangeMax){
+        claw = hardwareMap.get(Servo.class, Name);
+        claw.scaleRange(rangeMin, rangeMax);
+        if(reverse){
+            claw.setDirection(Servo.Direction.REVERSE);
+        }
+    }
+    public clawServo(HardwareMap hardwareMap, String Name, boolean reverse){
+        this(hardwareMap, Name, reverse, 0.0, 1.0);
+    }
+    public clawServo(HardwareMap hardwareMap, String Name){
+        this(hardwareMap, Name, false);
+    }
+    public void update(boolean open, boolean close){
+        if(open){
+            claw.setPosition(1.0);
+        }
+        if (close){
+            claw.setPosition(0.0);
+        }
+    }
+    boolean isOpen(){
+        return claw.getPosition()==1.0;
+    }
+    boolean isClosed(){
+        return claw.getPosition()==0.0;
+    }
+
+}
---
+++ linearSlide.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.Arrays;
+public class linearSlide {
+    private DcMotorEx slider;
+    private int[] StoppingPoints;
+    PIDFcontroller controller;
+    private int Pointer; //A POINTer to the stopping POINTS
+    private int NumberOfPoints;
+
+    public linearSlide(HardwareMap hardwareMap, String Name, int[] points, int currIndex, PIDFcontroller PID){
+        slider = hardwareMap.get(DcMotorEx.class, Name);
+        StoppingPoints = points;
+        Arrays.sort(StoppingPoints); // Allows us to use ++ and -- to move through the points
+        NumberOfPoints = StoppingPoints.length;
+        controller = PID;
+        Pointer = currIndex;
+    }
+    public linearSlide(HardwareMap hardwareMap, String Name, int[] points, int currIndex){
+        this(hardwareMap, Name, points, currIndex, new PIDFcontroller(0.0,0.0,0.0,0.0,0.0,10,5.0) );
+    }
+    public linearSlide(HardwareMap hardwareMap, String Name, int[] points){
+        this(hardwareMap,Name, points, 0);
+    }
+    public void update(boolean up, boolean down, boolean highest, boolean bottom){
+        if(highest){
+          controller.CalculateAsnyc(StoppingPoints[Pointer=NumberOfPoints],slider.getCurrentPosition());
+        }
+        if (bottom){
+            controller.CalculateAsnyc(StoppingPoints[Pointer=0],slider.getCurrentPosition());
+        }
+        if(Pointer != NumberOfPoints && up) {
+            controller.CalculateAsnyc(StoppingPoints[++Pointer], slider.getCurrentPosition());
+        }
+        if (Pointer != 0 && down){
+            controller.CalculateAsnyc(StoppingPoints[--Pointer], slider.getCurrentPosition());
+        }
+    }
+    boolean isTop(){
+        return Pointer == NumberOfPoints;
+    }
+    boolean isBottom(){
+        return Pointer == 0;
+    }
+}
